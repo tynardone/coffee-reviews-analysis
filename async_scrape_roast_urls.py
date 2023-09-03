d4ead09f2f4af231aaf026db47fc4da15c2b4231
Tyler Nardone <tynardone@gmail.com>
2023-09-03 17:09:12 -0400

    Progress bar working

--- async_scrape_roast_urls.py
+++ async_scrape_roast_urls.py
@@ -1,6 +1,7 @@
 from requests_html import AsyncHTMLSession
 import asyncio
 import pickle
+from tqdm import tqdm
 
 BASE_URL = 'https://www.coffeereview.com/review/page/'
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
@@ -9,26 +10,28 @@ FEATURE_LIST = ['Roaster Location', 'Coffee Origin', 'Roast Level', 'Aroma', 'Ac
                 'Bottom Line', 'Est. Price']
 TOTAL_PAGES = 367
 
-# Function to retrieve and pars out review urls
-async def scrape_review_list(session: AsyncHTMLSession, url: str):
+async def scrape_review_list(session: AsyncHTMLSession, url: str, pbar:tqdm):
     r = await session.get(url)
     links = r.html.links
     links = [l for l in links if '/review/' in l and '/page/' not in l]
     if r.status_code == 429 or r.status_code == 504:
         await asyncio.sleep(3)  # You can adjust the delay time (in seconds) as needed
-        return await scrape_review_list(session, url)
+        return await scrape_review_list(session, url, pbar)
     else:
+        pbar.update()
         return links
     
-async def main(urls):
+async def main(urls, pbar):
     session = AsyncHTMLSession()
-    tasks = [scrape_review_list(session, url) for url in urls]
+    tasks = [scrape_review_list(session, url, pbar) for url in urls]
     return await asyncio.gather(*tasks)
            
 if __name__ == '__main__':
    links = []
    urls = [BASE_URL + '{}/'.format(page_number) for page_number in range(1, TOTAL_PAGES)]
-   results = asyncio.run(main(urls))
+   pbar = tqdm(total=len(urls))
+   results = asyncio.run(main(urls, pbar))
+   pbar.close()
    flat_list = [item for sublist in results for item in sublist]
    print("Found {} URLS".format(len(flat_list)))
    
