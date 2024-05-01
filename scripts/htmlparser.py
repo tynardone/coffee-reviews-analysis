843d3132c987fc7cb630c42e1c90f1740d3b46f4
Tyler Nardone <tynardone@gmail.com>
2024-05-01 11:46:22 -0400

    parse_html working and helper functions facotred out

--- scripts/htmlparser.py
+++ scripts/htmlparser.py
@@ -1,5 +1,5 @@
 import logging
-from dataclasses import dataclass
+import re
 from pathlib import Path
 from bs4 import BeautifulSoup
 
@@ -11,30 +11,66 @@ current_file = Path(__file__)
 root_dir = current_file.parent.parent
 html_dir = root_dir / 'tests' / 'html'
 
+def _parse_tables(soup: BeautifulSoup) -> dict:
+    """Extracts data from all tables."""
+    data = {}
+    tables = soup.find_all('table')
+    for table in tables:
+        for row in table.find_all('tr'):
+            cells = row.find_all('td')
+            if len(cells) == 2:
+                data[cells[0].string.strip()] = cells[1].string.strip()
+    return data
+
+def _parse_class(soup: BeautifulSoup, element: str, class_: str) -> str:
+    """Extracts data from a specific class."""
+    try:
+        return soup.find(element, class_=class_).string.strip()
+    except AttributeError:
+        return None
+
+def _parse_string_next(soup: BeautifulSoup, find_element: str, next_element:str, string: str, ) -> str:
+    """Extracts data from a specific string."""
+    try:
+        return (soup
+                .find(find_element, string= re.compile(string))
+                .find_next(next_element)
+                .get_text().strip()
+        )
+    except AttributeError:
+        return None
+
 def parse_html(html: str) -> dict:
     """ Parse HTML content and return a dictionary of key-value pairs."""
     data = {}
     soup = BeautifulSoup(html, 'html.parser')
-    # rating
-    # roaster
-    # title
-    # roaster location
-    # coffee origin
-    # roast level
-    # agtron
-    # est price
-    # review date
-    # aroma
-    # acidit/sructure
-    # acidity
-    # body
-    # flavor
-    # aftertaste
-    # blind assessment
-    # notes
-    # bottom line
+
+    rating = _parse_class(soup, 'p', 'review-rating')
+    roaster = _parse_class(soup, 'p', 'review-roaster')
+    title = _parse_class(soup, 'h1', 'review-title')
+
+    blind_assessment = _parse_string_next(soup, 'h2', 'p', 'Blind Assessment')
+    notes = _parse_string_next(soup, 'h2', 'span', 'Notes')
+    bottom_line = _parse_string_next(soup, 'h2', 'p', 'Bottom Line')
+
+    table_data = _parse_tables(soup)
+
+    data['rating'] = rating
+    data['roaster'] = roaster
+    data['title'] = title
+    data['blind assessment'] = blind_assessment
+    data['notes'] = notes
+    data['bottom line'] = bottom_line
+    data.update(table_data)
     
     return data
 
+
 if __name__ == '__main__':
     html_files = [file for file in html_dir.glob('*.html')]
+    for file in html_files:
+        with open(file, 'r', encoding='utf-8') as f:
+            html = f.read()
+            data = parse_html(html)
+            print(data)
+            
