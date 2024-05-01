58d86cf622276ed3fc8812f93b08db76b06e412b
Tyler Nardone <tynardone@gmail.com>
2024-04-30 20:27:36 -0400

    Updated parse_review_soup

--- scripts/review_parse.py
+++ scripts/review_parse.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %
 base_path = Path(__file__).parent.parent
 test_html_path = base_path / 'tests/test_review_parse.html'
 
-def parse_table_soup(soup: BeautifulSoup) -> dict:
+def parse_tables_soup(soup: BeautifulSoup) -> dict:
     """Parse tables from the HTML content and return a dictionary of key-value pairs."""
     table_data = {}
     tables = soup.find_all('table')
@@ -19,24 +19,26 @@ def parse_table_soup(soup: BeautifulSoup) -> dict:
         for row in rows:
             cols = row.find_all('td')
             if len(cols) >= 2:  # Ensure there are at least two columns
-                key = cols[0].text.strip()
-                value = cols[1].text.strip()
+                key = cols[0].string.strip()
+                value = cols[1].string.strip()
                 table_data[key] = value
     return table_data
 
-def find_text(soup, selector, attribute=None, find_next_selector=None, **kwargs):
-    """ Find text in a BeautifulSoup object and return it."""
+def find_string(soup, selector, attribute=None, find_next_selector=None, **kwargs):
+    """ Find string in a BeautifulSoup object and return it."""
     try:
         element = soup.find(selector, **kwargs)
         if attribute:
-            return getattr(element, attribute)().text.strip()
+            return getattr(element, attribute)().string.strip()
         if find_next_selector:
-            return element.find_next(find_next_selector).text.strip()
-        return element.text.strip()
+            next_element= element.find_next(find_next_selector)
+            if next_element:
+                return next_element.get_text().strip()
+        return element.string.strip()
 
     except AttributeError as e:
         logging.error(
-           "Failed to find text for %s with attributes %s or error in %s: %s",
+           "Failed to find string for %s with attributes %s or error in %s: %s",
            selector, kwargs, find_next_selector, e
            )
         return None
@@ -44,16 +46,16 @@ def find_text(soup, selector, attribute=None, find_next_selector=None, **kwargs)
 def parse_review_soup(soup: BeautifulSoup) -> dict:
     """ Parse HTML content and return a dictionary of key-value pairs."""
     data = {
-        'rating': find_text(soup, 'div', class_='review-template-rating'),
-        'roaster': find_text(soup, 'div', class_='review-roaster'),
-        'name': find_text(soup, 'h1', class_='review-title'),
-        'blind_assessment': find_text(soup, 'h2', text='Blind Assessment',
+        'rating': find_string(soup, 'span', class_='review-template-rating'),
+        'roaster': find_string(soup, 'p', class_='review-roaster'),
+        'name': find_string(soup, 'h1', class_='review-title'),
+        'blind_assessment': find_string(soup, 'h2', string='Blind Assessment',
                                            find_next_selector='p'
                                            ),
-        'notes': find_text(soup, 'h2', text='Notes', find_next_selector='p'),
-        'bottom_line': find_text(soup, 'h2', text='Bottom Line', find_next_selector='p')
+        'notes': find_string(soup, 'h2', string='Notes', find_next_selector='p'),
+        'bottom_line': find_string(soup, 'h2', string='Bottom Line', find_next_selector='p')
     }
-    data.update(parse_tables(soup))
+    data.update(parse_tables_soup(soup))
     return data
 
 def main() -> None:
